cb748ff400133fd6ea7780076cc853464c9d092b
sraibaby <somewallet@gmail.com>
2025-10-02 15:14:25 +0300

    fix close/back tma built-ins

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,23 +8,26 @@ import { useTheme } from '@/core/theme';
 import { CustomKeyboard } from './main/components/custom-keyboard';
 import { useKeyboardDetection } from '@/hooks/use-keyboard-detection';
 import { BackgroundLoadingIndicator } from '@/components/BackgroundLoadingIndicator';
+import { Page } from '@/components/Page';
 
 export default function Home() {
   const { colors } = useTheme();
   const { shouldBeCompact, isInputFocused } = useKeyboardDetection();
 
   return (
-    <div 
-      className={`flex flex-col 
-        ${!isInputFocused && shouldBeCompact ? '' : 'min-h-screen'}
-      `}
-      style={{ backgroundColor: colors.background, color: colors.text }}
-    >
-      <Header />
-      <Swap />
-      <Footer />
-      {isInputFocused ? <CustomKeyboard /> : null}
-      <BackgroundLoadingIndicator />
-    </div>
+    <Page back={false}>
+      <div 
+        className={`flex flex-col 
+          ${!isInputFocused && shouldBeCompact ? '' : 'min-h-screen'}
+        `}
+        style={{ backgroundColor: colors.background, color: colors.text }}
+      >
+        <Header />
+        <Swap />
+        <Footer />
+        {isInputFocused ? <CustomKeyboard /> : null}
+        <BackgroundLoadingIndicator />
+      </div>
+    </Page>
   );
 }
